6556f2225b7c5b83b1c7e6e0c03ec49a6d506c28
LAIMeverik <Artur.Lotfullin@urfu.me>
2022-11-13 16:40:49 +0500

    Lab7.Chess

---
+++ Lab7/README.md
@@ -0,0 +1,37 @@
+# Applied Programming 
+
+## Author: 
+Artur Lotfullin
+
+## Task:
+he first number is the vertical number (when counting from left to right), the second number is the horizontal number (when counting from bottom to top).
+
+Given natural numbers horizontal, vertical, attackhorizontal, attackvertical, each of which does not exceed eight.
+
+Required:
+
+a) Find out if the fields (horizontal, vertical) and (m, n) are fields of the same color.
+
+b) On the field (horizontal, vertical) is a queen, rook, bishop or knight (user must enter). Does it threaten the field (attackhorizontal, attackvertical)?
+
+c) Find out whether it is possible to get from the square (horizontal, vertical) to the square (attackhorizontal, attackvertical) with one move of the rook, queen or bishop (must be entered by the user). If not, then find out how it can be done in two moves (indicate the field to which the first move leads).
+## Program name:
+Chess
+
+## Description:
+The user enters four coordinates of two cells on the chessboard, then the user needs to select a piece.
+## Screenshot:
+![image](https://sun9-18.userapi.com/impg/9Z3x-UOQ3aftmHX6eiLmIUxelzkAkvHq-lh3XQ/PcDnbD2AsoU.jpg?size=1203x346&quality=96&sign=4f3c9463256b99980d8ca700e2553a8d&type=album)
+![image](https://sun9-37.userapi.com/impg/6lVhgLhC6zafAiiMhJ2sXJF_dTjYkvBBl0kpig/YDqQ5zzrmJ0.jpg?size=1203x337&quality=96&sign=0ff81c10b22ede8cdc9d308392051dd3&type=album)
+![image](https://sun9-22.userapi.com/impg/kjEfg7NW5ZeCZiDR15-2a90W37aX83i2AU6Eeg/nV18AeOASfQ.jpg?size=1203x324&quality=96&sign=f04d27993fe12599bf60664a518793d5&type=album)
+## How to use:
+
+To use the program, you need to follow these steps:
+1) Run the program
+2) Enter number
+3) Get the execution result
+
+## Comments from the developer:
+
+To develop this program, a development environment called PyCharm was used.
+Translation was performed using the Python. You can use any development environment to open the main.py source code file.
---
+++ Lab7/main.py
@@ -0,0 +1,122 @@
+import logging
+
+# Работа с логированием
+logger = logging.getLogger("Logger")
+logger.setLevel(logging.INFO)
+
+# Создан файл для логирования
+file_handler = logging.FileHandler("log.log")
+# Создание форматера отображающего дату, время, имя логгера, уровень и сообщение
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+while True:
+    # Проверка на ввод и соответствие данных
+    try:
+        horizontal = int(input('Введите координату клетки фигуры по горизонтали: '))
+        if horizontal > 8 or horizontal < 0:
+            print('Введены некорректные данные. Попробуйте снова.')
+            continue
+        vertical = int(input('Введите координату клетки фигуры по вертикали: '))
+        if vertical > 8 or vertical < 0:
+            print('Введены некорректные данные. Попробуйте снова.')
+            continue
+        attackhorizontal = int(input('Введите координату атакуемой клетки по горизонтали: '))
+        if attackhorizontal > 8 or attackhorizontal < 0:
+            print('Введены некорректные данные. Попробуйте снова.')
+            continue
+        attackvertical = int(input('Введите координату атакуемой клетки по вертикали: '))
+        if attackvertical > 8 or attackvertical < 0:
+            print('Введены некорректные данные. Попробуйте снова.')
+            continue
+        figure = int(input('''Какую фигуру вы хотите использовать?
+        1 -- Ферзь
+        2 -- Ладья
+        3 -- Слон
+        4 -- Конь
+        Ваш выбор: '''))
+        if figure > 4 or figure < 0:
+            print('Введены некорректные данные. Попробуйте снова.')
+            continue
+    except ValueError:
+        print('Введены некорректные данные. Попробуйте снова.')
+        continue
+
+    # Проверка на совпадение цвета
+    if (horizontal + vertical) % 2 == (attackhorizontal + attackvertical) % 2:
+        print('Они одного цвета --', end=' ')
+        if (horizontal + vertical) % 2 == 0:
+            print('белого')
+        else:
+            print('черного')
+    else:
+        print('Нет, они не одного цвета')
+
+    # Расстояние по горизонтали и вертикали
+    dx = abs(horizontal - attackhorizontal)
+    dy = abs(vertical - attackvertical)
+
+    # Проверка угрозы фигуры полю + второй ход
+    if figure == 1:     # Ферзь
+        if horizontal == attackhorizontal or vertical == attackvertical or dx == dy:
+            print(f'Ферзь угрожает полю ({attackhorizontal}; {attackvertical})')
+        else:
+            print(f'Ферзь не угрожает полю ({attackhorizontal}; {attackvertical})')
+            print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizontal}; {vertical})')
+    elif figure == 2:   # Ладья
+        if horizontal == attackhorizontal or vertical == attackvertical:
+            print(f'Ладья угрожает полю ({attackhorizontal}; {attackvertical})')
+        else:
+            print(f'Ладья не угрожает полю ({attackhorizontal}; {attackvertical})')
+            print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizontal}; {vertical})')
+    elif figure == 3:   # Слон
+        if dx == dy:
+            print(f'Слон угрожает полю ({attackhorizontal}; {attackvertical})')
+        else:
+            print(f'Слон не угрожает полю ({attackhorizontal}; {attackvertical})')
+            if (horizontal + vertical) % 2 != (attackhorizontal + attackvertical) % 2:
+                print(f'Слон никаким образом не может угрожать полю ({attackhorizontal}; {attackvertical})')
+            else:
+                attackhorizontal0, attackvertical0, attackhorizontal1, attackvertical1 = attackhorizontal, attackvertical, 0, 0
+                while 0 < attackhorizontal0 < 9 and 0 < attackvertical0 < 9:
+                    attackhorizontal0 += 1
+                    attackvertical0 += 1
+                    if abs(horizontal - attackhorizontal0) == abs(vertical - attackvertical0):
+                        attackhorizontal1 = attackhorizontal0
+                        attackvertical1 = attackvertical0
+                        break
+                attackhorizontal0 = attackhorizontal
+                attackvertical0 = attackvertical
+                while 0 < attackhorizontal0 < 9 and 0 < attackvertical0 < 9:
+                    attackhorizontal0 += 1
+                    attackvertical0 -= 1
+                    if abs(horizontal - attackhorizontal0) == abs(vertical - attackvertical0):
+                        attackhorizontal1 = attackhorizontal0
+                        attackvertical1 = attackvertical0
+                        break
+                attackhorizontal0 = attackhorizontal
+                attackvertical0 = attackvertical
+                while 0 < attackhorizontal0 < 9 and 0 < attackvertical0 < 9:
+                    attackhorizontal0 -= 1
+                    attackvertical0 += 1
+                    if abs(horizontal - attackhorizontal0) == abs(vertical - attackvertical0):
+                        attackhorizontal1 = attackhorizontal0
+                        attackvertical1 = attackvertical0
+                        break
+                attackhorizontal0 = attackhorizontal
+                attackvertical0 = attackvertical
+                while 0 < attackhorizontal0 < 9 and 0 < attackvertical0 < 9:
+                    attackhorizontal0 -= 1
+                    attackvertical0 -= 1
+                    if abs(horizontal - attackhorizontal0) == abs(vertical - attackvertical0):
+                        attackhorizontal1 = attackhorizontal0
+                        attackvertical1 = attackvertical0
+                        break
+                print(f'Чтобы за два хода попасть в это поле необходимо встать на поле ({attackhorizontal1}; {attackvertical1})')
+    else:   # Конь
+        if abs(dx - dy) == 1:
+            print(f'Конь угрожает полю ({attackhorizontal}; {attackvertical})')
+        else:
+            print(f'Конь не угрожает полю ({attackhorizontal}; {attackvertical})')
+    break
+logger.info('Program done')
